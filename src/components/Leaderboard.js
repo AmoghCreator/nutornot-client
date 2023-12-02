055a784d64df2349cadc485b223145ec510c41bc
AadiXC0DE <aadityaz2077@gmail.com>
2023-12-02 22:30:31 +0530

    Add Tailwind, Modify Leaderboard UI

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,33 +1,34 @@
-import {useState , useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
-import '../styles/main.css';	
-import '../styles/leaderboard.css';	
 
 function Leaderboard() {
+  const [ldrbrd, setldrbrd] = useState([]);
 
-  const [ldrbrd , setldrbrd] = useState([]);
-	useEffect(() => {
-		fetchNext();
-	}, []);
+  useEffect(() => {
+    fetchNext();
+  }, []);
+
   async function fetchNext() {
-	  let res = await axios.get("https://nutornot.el.r.appspot.com/leaderboard");
-	  console.log(res.data);
-	  setldrbrd(res.data);
-}
+    let res = await axios.get("https://nutornot.el.r.appspot.com/leaderboard");
+    console.log(res.data);
+    setldrbrd(res.data);
+  }
 
   return (
-    <div class="leaderboard">
-	<h1>Nut or Not</h1>
-	<div className="btn-container">	
-	  {ldrbrd.map((item, key) => 
-		  (
-			  <div className="ldrbrd-item">
-			  <h3>{key+1}</h3>
-			  <h3>{item.title}</h3> 
-			  <img src={item.link} alt="logo" />
-			  <h3>{item.score}</h3>
-			  </div>
-		  ))} </div>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-gray-800 mb-4">Nut or Not</h1>
+      <div className="overflow-auto w-full md:w-2/3 lg:w-1/2 xl:w-1/3">
+        {ldrbrd.map((item, key) => (
+          <div className="flex items-center justify-between bg-white border-b border-gray-200 p-4">
+            <span className="text-lg font-semibold">{key + 1}</span>
+            <div className="flex flex-col items-center">
+              <h3 className="text-md font-medium">{item.title}</h3>
+              <img src={item.link} alt="logo" className="h-44 w-44 object-cover " />
+            </div>
+            <span className="text-lg font-semibold">Score {item.score}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
