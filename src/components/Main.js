26fac6305aab270c328cfd1279389de0403ec408
AmoghCreator <amoghpreneur@gmail.com>
2023-12-04 21:45:44 +0530

    PvP implemented

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ import nutOrNot from "../images/NutOrNot.png";
 import GoogleAds from './AdComponent';
 
 function Main() {
-  const [img, setImg] = useState("https://source.unsplash.com/random");
+  const [img, setImg] = useState([{link: "https://source.unsplash.com/random/800x600", title: "https://source.unsplash.com/random/800x600"}, {link: "https://source.unsplash.com/random/800x600", title: "https://source.unsplash.com/random/800x600"}]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -14,23 +14,28 @@ function Main() {
 
   async function fetchNext() {
 	setLoading(true);
-    let res = await axios.get("https://nutornot.el.r.appspot.com/image");
-    console.log(res.data[0].link);
-    setImg(res.data[0]);
+    let res = await axios.get("https://nutornot.el.r.appspot.com/pvp");
+    console.log(res.data);
+    setImg(res.data);
 	setLoading(false);
   }
 
-  async function Nut() {
-	setLoading(true);
-    await axios.get(`https://nutornot.el.r.appspot.com/image/add/${img.title}`);
+  async function Nut(e) {
+    setLoading(true);
+    if(e.target.name === "one") {
+	await axios.get(`https://nutornot.el.r.appspot.com/image/add/${img[0].title}`);
+    }
+    else {
+	await axios.get(`https://nutornot.el.r.appspot.com/image/add/${img[1].title}`);
+    }
     fetchNext();
   }
 
-  async function Not() {
+  /*async function Not() {
 	setLoading(true);
     await axios.get(`https://nutornot.el.r.appspot.com/image/sub/${img.title}`);
     fetchNext();
-  }
+  }*/
 
   return (
 	<>
@@ -52,13 +57,14 @@ function Main() {
       ) : (
         <>
           <div className="mb-4 mt-6 md:w-96 w-88 h-2/3 flex items-center justify-center overflow-hidden rounded-lg shadow-lg">
-            <img src={img.link} alt="logo" className="w-full h-full object-cover" />
+            <img src={img[0].link} alt="logo" className="w-full h-full object-cover" onClick={Nut} name="one" />
+            <img src={img[1].link} alt="logo" className="w-full h-full object-cover" onClick={Nut} name="two" />
           </div>
-          <div className="flex space-x-4">
+	  {/* <div className="flex space-x-4">
             <button className="bg-blue-500 hover:bg-blue-700 text-white text-2xl font-bold py-4 px-8 rounded" onClick={Nut}>Nut</button>
             <h3 className="my-auto text-xl">OR</h3>
             <button className="bg-red-500 hover:bg-red-700 text-white text-2xl font-bold py-4 px-8 rounded" onClick={Not}>Not</button>
-          </div>
+          </div> */} 
         </>
       )}
     </div>
